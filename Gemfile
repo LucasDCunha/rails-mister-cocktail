d4e3c2d61a8f5f308ad0690672fe77a2b75c99d3
Lucas Cunha <lucasdinizcunha@uol.com.br>
2020-02-20 15:58:14 -0300

    Implement front-end

--- Gemfile
+++ Gemfile
@@ -63,3 +63,7 @@ gem 'rails-controller-testing', group: [ :test ]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
+
+# add faker for seed
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 require 'json'
 require 'open-uri'
+require 'faker'
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
 user = JSON.parse(user_serialized)
+
+Dose.delete_all
+puts 'Deleted all doses'
 
 Ingredient.delete_all
 puts 'Deleted all ingredients'
@@ -11,3 +16,21 @@ user['drinks'].each do |ingredient_hash|
   Ingredient.create(name: ingredient_hash['strIngredient1'].to_s)
 end
 puts 'created new Ingredients'
+
+Cocktail.delete_all
+puts 'Deleted all cocktail'
+
+20.times do
+  Cocktail.create(name: Faker::Movies::LordOfTheRings.character)
+end
+
+puts 'Created new cocktails'
+
+Cocktail.all.each do |cocktail|
+  rand(1..5).times do
+    dose = Dose.new(cocktail: cocktail, description: Faker::Movies::Hobbit.quote)
+    dose.ingredient = Ingredient.all[rand(0..99)]
+    dose.save!
+  end
+end
+puts 'Added doses to cocktails'
